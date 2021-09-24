98f46e280722c85b0f689efeadc51cd03dd674d6
Coder 79 <dev.ahmed.saeed@gmail.com>
2021-09-24 05:48:55 +0200

    add new Components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Home from "./containers/Home";
 import About from "./containers/About";
 import AppLayout from "./components/AppLayout";
 import { Route, Switch, withRouter } from "react-router-dom";
+import Watch from "./containers/Watch";
+import Search from "./containers/Search";
+
+const API_KEY = "AIzaSyCpGnl9FtGEDIfait5i0tqm_rdvgTrc2IA";
 
 class App extends React.Component {
-    componentDidMount() {
+  componentDidMount() {
     this.loadYoutubeApi();
   }
 
@@ -15,9 +19,9 @@ class App extends React.Component {
     script.src = "https://apis.google.com/js/client.js";
 
     script.onload = () => {
-      window.gapi.load('client', () => {
+      window.gapi.load("client", () => {
         window.gapi.client.setApiKey(API_KEY);
-        window.gapi.client.load('youtube', 'v3', () => {
+        window.gapi.client.load("youtube", "v3", () => {
           this.props.youtubeLibraryLoaded();
         });
       });
@@ -25,7 +29,6 @@ class App extends React.Component {
 
     document.body.appendChild(script);
   }
-}
 
   render() {
     return (
@@ -33,6 +36,14 @@ class App extends React.Component {
         <Switch>
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route
+            path="/results"
+            render={() => <Search key={this.props.location.key} />}
+          />
+          <Route
+            path="/watch"
+            render={() => <Watch key={this.props.location.key} />}
+          />
         </Switch>
       </AppLayout>
     );
